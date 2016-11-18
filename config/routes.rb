3e20cc202caeae06f3fb7ca2f0baa35ec82760bb
barnagit <barna.hajzer@gmail.com>
2016-11-18 13:01:14 +0100

    sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :user_sessions, only: [:create, :destroy]
 
-  delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+  get '/sign_out', to: 'user_sessions#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
   post '/guests/create', to:'guests#create'
   
